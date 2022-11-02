9492e6625b68665ee903d3e27f07bdfbd2cd4737
Farhod <faha.azizov@gmail.com>
2022-11-02 23:09:44 +0500

    Change REST ENDPOINT

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,9 @@ const path = require('path');
 const isProd = process.env.NODE_ENV === 'production';
 
 const CONSTANTS = {
-  REST_ENDPOINT: JSON.stringify('https://paybay.kz/wp-json/wp/v2'),
+  REST_ENDPOINT: JSON.stringify('https://info-smartwatch.ru/wp-json/wp/v2'),
   POSTS_PER_PAGE: JSON.stringify(9),
-  GA_TRACKING_ID: JSON.stringify(isProd ? 'UA-111438908-1' : 'XXX'),
+  GA_TRACKING_ID: JSON.stringify(isProd ? 'XXX' : 'XXX'),
   REQUEST_CACHE_MAX: JSON.stringify(150)
 };
 
